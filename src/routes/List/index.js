6ddeed2eb886d7e483d436696e55a9a5e42241d0
Andrew Herriot <agherriot@gmail.com>
2020-01-20 21:05:10 -0500

    Prompt users to add more chips

--- src/routes/List/index.js
+++ src/routes/List/index.js
@@ -8,6 +8,7 @@ import geosStore from 'store/geos'
 import Header from 'components/Header'
 import Breadcrumb from 'components/Breadcrumb'
 import TextInput from 'components/TextInput'
+import Link from 'components/Link'
 
 import ListCell from './ListCell'
 import NewChipButton from './NewChipButton'
@@ -48,17 +49,23 @@ const List = view(({ match: { params: { geoId } } }) => {
           { url: `/chips/${geoId}`, title: geosStore.byId[geoId]?.title ?? '' }
         ]}
       />
-      <StyledSearchWrapper>
+      <StyledContent>
         <TextInput
           placeholder='Filter Chips'
           value={searchText}
           onChange={e => setSearchText(e.target.value)}
         />
-      </StyledSearchWrapper>
+      </StyledContent>
       {filteredChips.length === 0 && <StyledP>No chips match filter.</StyledP>}
       {filteredChips.map((chip, i) => (
         <ListCell key={chip.id} geoId={geoId} rank={i + 1} {...chip} />
       ))}
+      <StyledContent>
+        <p>
+          Is your favourite chip missing?{' '}
+          <Link to={`/chips/${geoId}/new`}>Add it</Link>
+        </p>
+      </StyledContent>
       <NewChipButton geoId={geoId} />
     </div>
   )
@@ -68,15 +75,18 @@ List.propTypes = {
   match: PropTypes.object.isRequired
 }
 
-const StyledSearchWrapper = styled.div`
+const StyledContent = styled.div`
   padding: 0 16px 16px;
   max-width: 400px;
   margin: 0 auto;
+  text-align: center;
 `
 
 const StyledP = styled.p`
   text-align: center;
   margin: 16px 0;
+  font-weight: bold;
+  font-size: 18px;
 `
 
 export default List
